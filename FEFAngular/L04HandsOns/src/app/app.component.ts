039fc6f27c620b1cc2090e16f592d4206e93b9b5
Rukshan Jonny Uddin <rukshan.uddin@gmail.com>
2021-03-07 17:56:19 -0500

    l04 ng

---
+++ FEFAngular/L04HandsOns/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "Rukshan Uddin";
+  city = "Palm Beach Gardens";
+  tagline =
+    "Full Stack Web Developer | React.js | Node.js | Javascript | Ruby on Rails | Postgres";
+  aboutMe =
+    "Full stack web developer with a background in business and technology and passion for coding. With experience in Ruby on Rails, JavaScript, and React, I bring strong skills in team-building and problem solving that help companies increase efficiency and productivity.";
+}
